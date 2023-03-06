666790961a0f87162064be528217bb828f381e8d
arezoo <arezoo.rahmani@harver.com>
2023-03-06 09:13:17 +0100

    convert text to json in /ping request add / info to show request info

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const app = express();
 
+//in post request if I want to access to req.body, I should first add yhis line
+app.use(express.json());
+//app.use('router',router)
+
 
 app.get('/', (req, res) => {
-    res.send('Home'); 
+    res.send('Got a GET request'); 
+});
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request'); 
 });
 
 app.get('/ping', (req, res) => { 
-    res.send('{ pong: 1 }')
+       res.cookie('name', 'ar-coockie', { domain: '.example.com', path: '/ping', secure: true, httpOnly: true })
+    res.json({ pong: 1 })
 });
 
-
-
+app.get('/info', (req, res) => { 
+    console.log(req.protocol)
+    console.log(req.hostname)
+    console.log(req.path)
+    console.log(req.method)
+    ///???does not show in response??
+    res.send('Protocol is :',req.protocol)
+    res.send('Host name is :',req.hostname)
+    res.send('path is :', req.path)
+    res.send('method is :', req.method)
+    res.send('Status Code is :',res.statusCode)
+    res.send('Status Message is :',res.statusMessage) 
+    
+});
 
 app.listen(12345, () => console.log('My App is listening on port 12345.'));
