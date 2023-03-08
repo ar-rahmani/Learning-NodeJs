73195ab2d40e23b9cf5b22f80e64d0a413bc611e
arezoo <arezoo.rahmani@harver.com>
2023-03-08 05:19:34 +0100

    task2: /ping-pong

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-//in post request if I want to access to req.body, I should first add yhis line
+
 app.use(express.json());
-//app.use('router',router)
 
 
 app.get('/', (req, res) => {
@@ -15,7 +14,6 @@ app.post('/', (req, res) => {
 });
 
 app.get('/ping', (req, res) => { 
-       res.cookie('name', 'ar-coockie', { domain: '.example.com', path: '/ping', secure: true, httpOnly: true })
     res.json({ pong: 1 })
 });
 
@@ -24,14 +22,6 @@ app.get('/info', (req, res) => {
     console.log(req.hostname)
     console.log(req.path)
     console.log(req.method)
-    ///???does not show in response??
-    res.send('Protocol is :',req.protocol)
-    res.send('Host name is :',req.hostname)
-    res.send('path is :', req.path)
-    res.send('method is :', req.method)
-    res.send('Status Code is :',res.statusCode)
-    res.send('Status Message is :',res.statusMessage) 
-    
 });
 
 app.listen(12345, () => console.log('My App is listening on port 12345.'));
