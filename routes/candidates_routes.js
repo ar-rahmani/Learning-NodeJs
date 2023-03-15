4770475cfcdb56de40b1ae9b6554c0d67617d050
ar-rahmani <50565121+ar-rahmani@users.noreply.github.com>
2023-03-15 19:39:59 +0100

    Revert "Create an endpoint that POST /candidate (body: { name: string }) save…"

--- routes/candidates_routes.js
+++ routes/candidates_routes.js
@@ -1,32 +1,17 @@
 const express = require('express')
-const bodyparser = require('body-parser')
-const mongodb = require('mongodb')
-
-const { database } = require('../mongo')
 const c_router = express.Router()
 
 c_router
-    .get('/:candidateId', async function(req, res, next) {
-    // let a1 =req.method
-    // let a2 = req.params
-    //     res.send(`uri is : ${a2} \n method is : ${a1} `)
-    //    // console.log(database.db)
-        const collection = database.db.collection("candidate")
-        const cur = collection.find({}, {},{},{})
-        
-        let item = []
-        await cur.forEach(function (doc) {
-            item.push(doc)
-        });
-        res.end(JSON.stringify(item))
+    .get('/:candidateId', (req, res, next) => {
+    let a1 =req.method
+    let a2 = req.params
+        res.send(`uri is : ${a2} \n method is : ${a1} `)
         next()
     }) 
     
-    .post('/:candidateId', async function (req, res, next) {  
-        const collection = database.db.collection("candidate")
-        const rest = await collection.insertOne(req.body);
+    .post('/:candidateId', (req, res, next) => {
         res.send(req.body)
         next()
-    })
+})  
 
-    module.exports = c_router
+module.exports = c_router
