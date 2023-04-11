215e61b7c446325ea1e6cd5fc0b8e686f81067e2
arezoo <arezoo.rahmani@harver.com>
2023-04-11 16:57:21 +0200

    Added Mongoose used mongoose insted of mongodb, and changed my code to use mongoose added a new route to insert candidate Note and sanitize candidate's Note connect candidate and candidateNote collection by candidateId as a ref

--- routes/candidates_routes.js
+++ routes/candidates_routes.js
@@ -1,41 +1,74 @@
 const express = require('express')
 const bodyparser = require('body-parser')
-const mongodb = require('mongodb')
+const mongoose = require('mongoose')
+const sanitizeHtml = require('sanitize-html')
+const candidateNoteModel = require('../models/candidateNoteModel')
+const candidateModel = require('../models/candidateModel')
 
-const { database } = require('../mongo')
+
 const c_router = express.Router()
 
 c_router
-    .get('/retrieve', async function(req, res, next) {
-        const collection = database.db.collection("candidate")
-        const items = await collection.find({}).toArray()
-        res.send(items)
+    .get('/retrieve', async function (req, res, next) {
+        const all = await candidateModel.find({})
+        res.send(all)       
         next()
-    }) 
+    })
     
-    .get('/:candidateId', async function(req, res, next) {
-        const collection = database.db.collection("candidate")
-        const o_id  = req.params.candidateId
-        const item = await collection.findOne({ candidateId: o_id })
-        return (item) ? res.send(item) : res.status(404).send('Not found') 
+    .get('/:candidateId', async function (req, res, next) {
+        const item = await candidateModel.findById({ _id: req.params.candidateId })
+        console.log(item)
+        if (item) {
+            res.send(item)
+        }
+        else { //inja error dare vaghti item ro peida nemikone
+            res.status(404).send('Not found')
+        }
         next()
     })
 
-     .post('/update', async function (req, res, next) {  
-        const collection = database.db.collection("candidate")
-        const fname = req.body.candidate_firstname
-        const cid = req.body.candidateId
-        const item = await collection.findOne({ candidate_firstname: fname })
-        if (item) collection.updateOne({ "candidate_firstname": fname }, { $set: { "candidateId": cid } });
-        else  res.status(404).send('Notfound') 
+    .post('/update/:candidateId', async function (req, res, next) {
+        try {
+            const doc = await candidateModel.findOneAndUpdate(
+            { _id : req.params.candidateId },
+            { candidate_firstname : req.body.candidate_firstname },
+            { new : true },
+            )   
+            res.send(doc)
+        }
+        catch (error) {
+            res.status(500).json({ message : err.message })
+        }
         next()
     })
 
-    .post('/:candidateId', async function (req, res, next) {  
-        const collection = database.db.collection("candidate")
-        const rest = await collection.insertOne(req.body);
-        res.send(req.body)
+    .post('/:candidateId', async function (req, res, next) {
+        const cnd = new candidateModel(req.body)
+        try {
+            const inscnd = await cnd.save()
+            res.send(cnd)
+        }
+        catch (error) {
+            res.status(400).json({message : err.message})
+        }
         next()
     })
+
+    .post('/:candidateId/note', async (req, res, next) => {
+        const clean = sanitizeHtml(req.body.description)
+        const note = new candidateNoteModel({
+            description : clean,
+            candidate_id: req.params.candidateId
+        })
+        try {
+            const result = await note.save()
+            res.send(result)
+        }
+        catch (error) {
+            res.status(400).json({ message: error.message })
+        }
+    next()
+
+})
 
     module.exports = c_router
